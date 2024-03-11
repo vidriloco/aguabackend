51b07d4a978379209dac43c105b62307b8db670c
Alejandro Cruz <vidriloco@gmail.com>
2024-03-10 22:22:42 -0600

    Completed first version of aguas con el agua

--- geodjango/geodjango/urls.py
+++ geodjango/geodjango/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from django.urls import path
 from world import api
-from world.views import mapbox_map_view
+from world.views import sources_map_view
+from world.views import disrupted_areas_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('settlements', api.settlements),
-    path('mapbox-map', mapbox_map_view, name='mapbox_map_view')
+    path('fuentes-de-abastecimiento', sources_map_view, name='sources_map'),
+    path('colonias-afectadas', disrupted_areas_view, name='disrupted_areas')
 ]
--- geodjango/world/views.py
+++ geodjango/world/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from .models import Settlement
 
-def mapbox_map_view(request):
+def sources_map_view(request):
     settlements = Settlement.objects.all()
-    return render(request, 'mapbox_map.html', {'settlements': settlements})
+    return render(request, 'sources_map.html', {'settlements': settlements})
+
+def disrupted_areas_view(request):
+    settlements = Settlement.objects.all()
+    return render(request, 'disrupted_areas.html', {'settlements': settlements})
