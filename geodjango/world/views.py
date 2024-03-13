7443c7e61c153470baf01e7a93dc4ad48ec933e8
Alejandro Cruz <vidriloco@gmail.com>
2024-03-13 00:38:41 -0600

    Fetch yesterday data if today data is not ready yet

--- geodjango/world/views.py
+++ geodjango/world/views.py
@@ -4,11 +4,16 @@ import datetime
 from datetime import timedelta
 
 def sources_map_view(request):
-    today = datetime.date.today()
-    settlements = Settlement.objects.filter(day_measured=today)
-    return render(request, 'sources_map.html', {'settlements': settlements})
+    return render(request, 'sources_map.html', { 'settlements': settlements() })
 
 def disrupted_areas_view(request):
+    return render(request, 'disrupted_areas.html', { 'settlements': settlements() })
+
+def settlements():
     today = datetime.date.today()
     settlements = Settlement.objects.filter(day_measured=today)
-    return render(request, 'disrupted_areas.html', {'settlements': settlements})
+
+    if not settlements:
+        yesterday = today - timedelta(days=1)
+        settlements = Settlement.objects.filter(day_measured=yesterday)
+    return settlements
