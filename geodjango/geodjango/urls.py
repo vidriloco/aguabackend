222b13b56ab43122dd9cb3dbde09376bbf07d94b
Alejandro Cruz <vidriloco@gmail.com>
2024-03-12 23:25:42 -0600

    Update Django redirect URL from root to default

--- geodjango/geodjango/urls.py
+++ geodjango/geodjango/urls.py
@@ -3,8 +3,10 @@ from django.urls import path
 from world import api
 from world.views import sources_map_view
 from world.views import disrupted_areas_view
+from django.shortcuts import redirect
 
 urlpatterns = [
+    path('', lambda req: redirect('/colonias-afectadas')),
     path('admin/', admin.site.urls),
     path('settlements', api.settlements),
     path('fuentes-de-abastecimiento', sources_map_view, name='sources_map'),
